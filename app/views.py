4dc6bc1a3156ff0a9f2fe858b7bf6d63777ab325
ranjithkumark9989 <ranjithkumark9989@gmail.com>
2022-11-22 13:10:20 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def vikram (request):
+    return HttpResponse('vikram is go to out')
+def wish(request):
+    return HttpResponse('happy birthday to u darling')
